c8d6932b6bf34d308f0c6cf1a93d911d79c756d9
Markus Gothe <nietzsche@lysator.liu.se>
2015-09-21 19:43:26 +0200

    Add MIPS64 stubs

---
+++ src/nc/arch/mips64/CallingConventions.h
@@ -0,0 +1,23 @@
+/* The file is part of Snowman decompiler. */
+/* See doc/licenses.asciidoc for the licensing information. */
+
+#pragma once
+
+#include <nc/config.h>
+
+#include <nc/core/ir/calling/Convention.h>
+
+namespace nc {
+namespace arch {
+namespace mips {
+
+class MipsArchitecture;
+
+class o32CallingConvention: public core::ir::calling::Convention {
+public:
+    o32CallingConvention(const MipsArchitecture *architecture_);
+};
+
+}}} // namespace nc::arch::mips
+
+/* vim:set et sts=4 sw=4: */
---
+++ src/nc/arch/mips64/Mips64Disassembler.h
@@ -0,0 +1,41 @@
+/* The file is part of Snowman decompiler. */
+/* See doc/licenses.asciidoc for the licensing information. */
+
+#pragma once
+
+#include <nc/config.h>
+
+#include <memory>
+
+#include <nc/core/arch/Capstone.h>
+#include <nc/core/arch/Disassembler.h>
+
+namespace nc {
+namespace arch {
+namespace mips {
+
+class MipsArchitecture;
+
+/**
+ * Disassembler for MIPS architecture.
+ *
+ * TODO: Support for MIPS16 instructions.
+ */
+class MipsDisassembler: public core::arch::Disassembler {
+    std::unique_ptr<core::arch::Capstone> capstone_;
+    int mode_;
+
+public:
+    /**
+     * \param architecture Valid pointer to the MIPS architecture.
+     */
+    MipsDisassembler(const MipsArchitecture *architecture);
+
+    virtual ~MipsDisassembler();
+
+    std::shared_ptr<core::arch::Instruction> disassembleSingleInstruction(ByteAddr pc, const void *buffer, ByteSize size) override;
+};
+
+}}} // namespace nc::arch::mips
+
+/* vim:set et sts=4 sw=4: */
