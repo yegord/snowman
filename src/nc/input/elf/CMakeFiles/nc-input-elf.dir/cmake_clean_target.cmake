965a95e8b04a30fa55905cf48508e505c241e2cb
Markus Gothe <nietzsche@lysator.liu.se>
2015-11-08 15:07:27 +0100

    Add support for addend2 on SPARC.

--- src/nc/input/elf/CMakeFiles/nc-input-elf.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libnc-input-elf.a"
-)
--- src/nc/input/elf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nietzsche/Downloads/snowman/src/nc/input/elf
-# Build directory: /Users/nietzsche/Downloads/snowman/src/nc/input/elf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
